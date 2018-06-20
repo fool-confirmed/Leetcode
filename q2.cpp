25f67e89bb30aa593699f2ece80ded14cdcf587a
fool-confirmed <31010979+fool-confirmed@users.noreply.github.com>
2018-06-20 12:35:15 -0400

    fixed bugs in q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -7,21 +7,32 @@ struct ListNode {
     ListNode(int x) :val(x), next(0) {}
 };
 
+/*
+// how to get Leetcode tests to run approximately 10-40% faster, since they do a lot of print outs.
+static auto x = [](){
+    // turn off sync
+    std::ios::sync_with_stdio(false);
+    // untie in/out streams
+    std::cin.tie(NULL);
+    return 0;
+}();
+*/
+
 class Solution {
 public:
+    // 39ms
     ListNode* addTwoNumbers(ListNode* L1, ListNode* L2){
         ListNode *returnNode = 0, *currentNode = 0, *newNode;
         int moveUp = 0, sum = 0, counter = 0;
 
-        while(counter < 10 && (L1 || L2)){
-            counter++;
+        while(moveUp || L1 || L2){
             int left = L1 ? (*L1).val : 0;
             int right = L2 ? (*L2).val : 0;
-//std::cout << L1 << ", " << L2 << "\n";
-            sum = (left + right) % 10 + moveUp;
-            moveUp = (left + right) >= 10 ? 1 : 0;
+//std::cout << left << ", " << right << "\n";
+            sum = (left + right + moveUp) % 10;
+            moveUp = (left + right + moveUp) >= 10 ? 1 : 0;
             newNode = new ListNode(sum);
-std::cout << "new node @: " << newNode << "\n";
+//std::cout << "new node @: " << newNode << "\n";
             // new node becomes current, or current.next
             if(currentNode){
                 (*currentNode).next = newNode;
@@ -29,7 +40,7 @@ std::cout << "new node @: " << newNode << "\n";
             }else{
                 currentNode = newNode;
             }
-std::cout << "current node @: " << currentNode << "\n";
+//std::cout << "current node @: " << currentNode << "\n";
             // assign current to return if return is empty
             // only need this once
             if(returnNode){
@@ -41,7 +52,7 @@ std::cout << "current node @: " << currentNode << "\n";
             // advance input lists
             L1 = L1 && (*L1).next ? (*L1).next : 0;
             L2 = L2 && (*L2).next ? (*L2).next : 0;
-std::cout << "sum, r: " << sum << ", " << moveUp << " @ " << &newNode << "\n";
+//std::cout << "sum, r: " << sum << ", " << moveUp << " @ " << &newNode << "\n";
         }
 
         return returnNode;
@@ -56,7 +67,7 @@ int main(){
     ListNode l14 = ListNode(1);
     l11.next = &l12;
     l12.next = &l13;
-    l13.next = &l14;
+    //l13.next = &l14;
 
     ListNode l21 = ListNode(5);
     ListNode l22 = ListNode(6);
@@ -64,10 +75,15 @@ int main(){
     l21.next = &l22;
     l22.next = &l23;
 
+    ListNode t1 = ListNode(1);
+    ListNode t2 = ListNode(9);
+    ListNode t21 = ListNode(9);
+    t2.next = &t21;
+
     Solution foo = Solution();
     ListNode* rl = foo.addTwoNumbers(&l11, &l21);
 
-std::cout << "rl: " << rl << '\n';
+//std::cout << "rl: " << rl << '\n';
 
     while(rl){
         std::cout << "rl val: " << (*rl).val << ' ';
