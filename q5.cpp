2dc7d11b9f63d0ecb23ea085c117002685c4e95d
fool-confirmed <31010979+fool-confirmed@users.noreply.github.com>
2018-06-30 16:31:52 -0400

    Refactored q5.cpp to pass all tests

--- q5.cpp
+++ q5.cpp
@@ -12,46 +12,38 @@ static auto x = [](){
 */
 
 int isPalindromic(std::string s) {
-	int len = s.length();
-	if (len % 2 == 0) {
-		//even
-		for (int i = 0; i < len / 2 ; i++) {
-			if (s.at(i) != s.at(len - 1 - i)) {
-				return 0;
-			}
+	int len = s.length(); //std::cout << s << '\n';
+	for (int i = 0; i < len / 2; i++) {
+		if (s.at(i) != s.at(len - 1 - i)) {
+			return 0;
 		}
 	}
-	else {
-		//odd
-		for (int i = 0; i < len / 2 ; i++) {
-			if (s.at(i) != s.at(len - 1 - i)) {
-				return 0;
-			}
-		}
-	}
-
-
 	return s.length();
 }
 
 class Solution {
 public:
 	std::string longestPalindrome(std::string s) {
-		const char* arr = s.c_str();
-		int end, maxLen = 0;
+		if (s.length() == 1) { return s; }
+		int indexFound, maxLen = 0;
 		std::string retval;
 
 		for (int i = 0; i < s.length(); i++) {
-			end = strrchr(arr, arr[i]) - arr - i;std::cout << end << "\n";
-			if (end >= 0) {
-				int l = isPalindromic(s.substr(i, end + 1));
+			indexFound = s.find(s.at(i), i + 1);
+			while (indexFound != s.npos)
+			{
+				int l = isPalindromic(s.substr(i, indexFound - i + 1));
 				if (l > maxLen) {
 					maxLen = l;
-					retval = s.substr(i, end + 1);
+					retval = s.substr(i, indexFound - i + 1);
 				}
+				if (maxLen == s.length()) {
+					return s;
+				}
+				indexFound = s.find(s.at(i), indexFound + 1);
 			}
 		}
-
+		if (retval.empty()) { retval = s.at(0); }
 		return retval;
 	}
 };
@@ -61,10 +53,15 @@ int main() {
 
 	Solution s;
 
-	//std::cout << "bbbabb :" << s.longestPalindrome("bbbabb") << "\n";
-	//std::cout << "a :" << s.longestPalindrome("a") << "\n";
+	std::cout << "bbbabb :" << s.longestPalindrome("bbbabb") << "\n";
+	std::cout << "a :" << s.longestPalindrome("a") << "\n";
+	std::cout << "bb :" << s.longestPalindrome("bb") << "\n";
 	std::cout << "abcda :" << s.longestPalindrome("abcda") << "\n";
 	std::cout << "aaabaaaa :" << s.longestPalindrome("aaabaaaa") << "\n";
+	std::cout << "ranynar: " << s.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth") << "\n";
+
+	std::cout << "cbbd :" << s.longestPalindrome("cbbd") << "\n";
+
 	// print length if palindromic, or zero
 	/*std::cout << isPalindromic("babab") << "\n";
 	std::cout << isPalindromic("abccba") << "\n";
